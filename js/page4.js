1a89c793d7461504d1e358e42ec266f8a2ba6b28
BonnieLiu1207 <1105781646@qq.com>
2024-11-13 15:44:27 -0600

    Update page4.js

--- js/page4.js
+++ js/page4.js
@@ -10,25 +10,16 @@ photoDivs[currentPhotoNumber].classList.remove('hideThis');
 photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
 nextButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add("hideThis");
-  currentPhotoNumber = currentPhotoNumber + 1;
-
-  if (currentPhotoNumber === photoDivs.length) {
-    currentPhotoNumber = 0;
-  }
-
-  photoDivs[currentPhotoNumber].classList.remove('hideThis');
-  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+    photoDivs[currentPhotoNumber].classList.add("hideThis");
+    currentPhotoNumber = (currentPhotoNumber + 1) % photoDivs.length;
+    photoDivs[currentPhotoNumber].classList.remove('hideThis');
+    photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 });
 
 previousButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add("hideThis");
-  currentPhotoNumber = currentPhotoNumber - 1;
-
-  if (currentPhotoNumber < 0) {
-    currentPhotoNumber = photoDivs.length - 1;
-  }
-
-  photoDivs[currentPhotoNumber].classList.remove('hideThis');
-  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+    photoDivs[currentPhotoNumber].classList.add("hideThis");
+    currentPhotoNumber = (currentPhotoNumber - 1 + photoDivs.length) % photoDivs.length;
+    photoDivs[currentPhotoNumber].classList.remove('hideThis');
+    photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 });
+
